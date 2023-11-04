a2a76933fd5491001e388c7234a3f7fdd501df84
laraduarte2 <duartelara202@gmail.com>
2023-11-04 16:04:00 -0300

    logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ function MyAppBar() {
   return (
     <AppBar position="static" style={{backgroundColor:'#080a12'}}>
         <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
-            <img src="src/assets/logo.png" alt="Logo" style={{ width: '150px', height: 'auto' }}/>
+            <img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="Logo" style={{ width: '150px', height: 'auto' }}/>
         </Toolbar>
     </AppBar>
   );
